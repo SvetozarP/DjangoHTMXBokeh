a0548f07680a56344a070c5b3107d5027dd6e58b
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-05-28 20:48:16 +0100

    Created the management command to load the json in DB

--- gdp/management/commands/populate.py
+++ gdp/management/commands/populate.py
@@ -1,8 +1,29 @@
+import json
+import itertools
 from django.core.management.base import BaseCommand
+from gdp.models import GDP
+from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = 'Load Courses and Modules'
+    help = 'Load gdp.json to DB'
 
     def handle(self, *args, **kwargs):
-        raise NotImplementedError
+        if not GDP.objects.count():
+            datafile = settings.BASE_DIR / 'data' / 'gdp.json'
+            with open(datafile, 'r') as f:
+                data = json.load(f)
+
+            data = itertools.dropwhile(lambda x: x['Country Name'] != 'Afghanistan', data)
+
+            gdps = []
+
+            for d in data:
+                gdps.append((GDP(
+                    country=d['Country Name'],
+                    country_code=d['Country Code'],
+                    year=d['Year'],
+                    gdp=d['Value'],
+                )))
+
+            GDP.objects.bulk_create(gdps)
