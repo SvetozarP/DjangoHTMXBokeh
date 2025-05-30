246985f6328d15e816d1296139b39c58e675d4ab
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-05-30 22:02:20 +0100

    Created line and multiline charts

--- gdp/views.py
+++ gdp/views.py
@@ -51,3 +51,75 @@ def index(request):
         return render(request, 'partials/gdp-bar.html', context)
 
     return render(request, 'index.html', context)
+
+
+def line(request):
+    countries = GDP.objects.values_list('country', flat=True).distinct()
+    country = request.GET.get('country', 'Germany')
+
+    gdps = GDP.objects.filter(country=country).order_by('year')
+
+    # Multiline chart
+    year_data = []
+    gdp_data = []
+    co = ['Germany', 'China', 'France']
+
+    for count in co:
+        co_gdps = GDP.objects.filter(country=count).order_by('year')
+        ml_country_years = year_data.append([d.year for d in co_gdps])
+        ml_country_gdps = gdp_data.append([d.gdp for d in co_gdps])
+
+    # End of Multiline
+
+    country_years = [d.year for d in gdps]
+    country_gdps = [d.gdp for d in gdps]
+
+    cds = ColumnDataSource(data=dict(country_years=country_years, country_gdps=country_gdps))
+
+    # Multiline
+
+    cds_ml = ColumnDataSource(data=dict(country_years=year_data, country_gdps=gdp_data))
+
+    # end of Multiline
+
+    fig = figure(height=500, title=f'{country} GDP')
+    fig.title.align = 'center'
+    fig.title.text_font_size = '1.5em'
+    fig.yaxis[0].formatter = NumeralTickFormatter(format='$0.0a')
+
+    fig.line(source=cds, x='country_years', y='country_gdps', line_width=2)
+
+    # Multiline
+
+    fig2 = figure(height=500, title=f'Multiline GDP')
+    fig2.title.align = 'center'
+    fig2.title.text_font_size = '1.5em'
+    fig2.yaxis[0].formatter = NumeralTickFormatter(format='$0.0a')
+
+    fig2.multi_line(source=cds_ml, xs='country_years', ys='country_gdps', line_width=2)
+
+    script1, div1 = components(fig2)
+    # end of multiline
+
+    tooltips = [
+        ('country_year', '@country_years'),
+        ('GDP', '@country_gdps{,}'),
+    ]
+
+    fig.add_tools(HoverTool(tooltips=tooltips))
+
+    script, div = components(fig)
+
+    context = {
+        'countries': countries,
+        'country': country,
+        'script': script,
+        'div': div,
+        'div1': div1,
+        'script1': script1,
+    }
+
+    if request.htmx:
+        return render(request, 'partials/gdp-bar.html', context)
+
+    return render(request, 'line.html', context)
